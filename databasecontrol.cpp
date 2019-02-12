6d273d51b7e65b1e10903ea494f73367cd327fa3
ming <minglei@cmail.carleton.ca>
2019-02-11 22:36:03 -0500

    3004 D1 final version

--- databasecontrol.cpp
+++ databasecontrol.cpp
@@ -17,13 +17,18 @@ void DatabaseControl::initAnimals(){
         Animal *a1 = new  Animal (1001, "Luna", "Dog");
         Animal *a2 = new  Animal (1002, "Lancy", "Cat");
         Animal *a3 = new  Animal (1003, "Rich", "Dog");
+        Animal *a4 = new  Animal (1004, "Coupon", "Cat");
+        Animal *a5 = new  Animal (1005, "Cookie", "Cat");
         _db->addAnimal(a1);
         _db->addAnimal(a2);
         _db->addAnimal(a3);
+        _db->addAnimal(a4);
+        _db->addAnimal(a5);
         insertAnimal(a1);
         insertAnimal(a2);
         insertAnimal(a3);
-
+        insertAnimal(a4);
+        insertAnimal(a5);
 
 }
 
